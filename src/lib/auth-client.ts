b8ef14efb0679c3232a34829fbe9bf005e5fd066
lucasm-avila <lucasm.aavila@gmail.com>
2026-06-06 15:59:46 -0300

    remove local host url

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,3 @@
 import { createAuthClient } from 'better-auth/client'
 
-export const authClient = createAuthClient({
-  baseURL: 'http://localhost:3000',
-})
+export const authClient = createAuthClient()
